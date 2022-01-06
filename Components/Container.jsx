4f3b7084be712fb9b188b273f78ac788926939a0
nicoContigliani <nico.contigliani@gmail.com>
2022-01-06 14:04:41 -0300

    end?

--- Components/Container.jsx
+++ Components/Container.jsx
@@ -30,7 +30,6 @@ const Container = (props) => (
         href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/spacelab/bootstrap.min.css"
       />
       <title>Nicolas Contigliani Portafolio</title>
-
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" referrerpolicy="no-referrer" />
 
       {/* <style src="../styles/Home.module.css"></style> */}
